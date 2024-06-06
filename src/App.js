141324e956082f21e72e76ce089d7d6601260b8f
Raymond IT Support <ServicedeskRetail@Raymond.in>
2024-06-06 20:10:39 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This Is My First Project Try to see
+          This Is My First Project 
         </p>
         <a
           className="App-link"
